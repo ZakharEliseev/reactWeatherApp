3e1067f1af6c76e3268dcb13b2c3df8072f37a13
Ваше Имя <you@example.com>
2025-11-21 19:11:37 +0500

    refactor

--- src/store/actions/weatherActions.tsx
+++ src/store/actions/weatherActions.tsx
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
+// eslint-disable-next-line import/order
+import {
+  FETCHING_WEATHER_FULFILLED,
+  FETCH_WEATHER,
+  FETCH_WEATHER_REJECTED,
+  GET_CITY_NAME,
+} from '../../types/constants';
 
 // eslint-disable-next-line import/order
-import { FETCHING_WEATHER_FULFILLED, FETCH_WEATHER, FETCH_WEATHER_REJECTED, GET_CITY_NAME } from '../../types/constants';
-
-
-// eslint-disable-next-line no-restricted-imports
-import 'dayjs/locale/ru';
-
-// eslint-disable-next-line import/order
-import {DateTimeService} from '../../services/dateTimeService';
-
+import { DateTimeService } from '../../services/dateTimeService';
 
 const dateTimeService = new DateTimeService();
 
@@ -19,40 +18,38 @@ export const actionGetCityName = (cityName: string) => ({
   payload: { cityName },
 });
 
-export const actionFetchWeatherApiData = (cityName: string) => (dispatch: any) => {
+export const actionFetchWeatherApiData = (cityName: string) => async (dispatch: any) => {
   dispatch({ type: FETCH_WEATHER });
-
-  axios
-    .get('https://api.openweathermap.org/data/2.5/forecast', {
+  try {
+    const response = await axios.get('https://api.openweathermap.org/data/2.5/forecast', {
       params: {
         q: cityName,
         appid: '286e99238dc4343e41f63ec35cf640ea',
         units: 'metric',
         lang: 'ru',
       },
-    })
-    .then((response) => {
-      const groupedWeatherData = response.data.list.reduce((acc: any, item: any) => {
-        const date = dateTimeService.format(item.dt_txt, 'YYYY-MM-DD');
-        const time = dateTimeService.format(item.dt_txt, 'HH:mm');
-        const entry = {
-          time,
-          temp: Math.ceil(item.main.temp),
-          description: item.weather[0].description,
-          icon: item.weather[0].icon,
-          wind: Math.ceil(item.wind.speed),
-          pressure: Math.ceil(item.main.pressure / 1.33),
-          humidity: item.main.humidity,
-        };
-        // eslint-disable-next-line no-param-reassign
-        if (!acc[date]) acc[date] = [];
-        acc[date].push(entry);
-        return acc;
-      }, {});
-      dispatch({ type: FETCHING_WEATHER_FULFILLED, payload: {groupedWeatherData} });
-    })
-    .catch((e) => {
-      dispatch({ type: FETCH_WEATHER_REJECTED , payload: {e}});
     });
+    const groupedWeatherData = response.data.list.reduce((acc: any, item: any) => {
+      const date = dateTimeService.format(item.dt_txt, 'YYYY-MM-DD');
+      const time = dateTimeService.format(item.dt_txt, 'HH:mm');
+      const entry = {
+        time,
+        temp: Math.ceil(item.main.temp),
+        description: item.weather[0].description,
+        icon: item.weather[0].icon,
+        wind: Math.ceil(item.wind.speed),
+        pressure: Math.ceil(item.main.pressure / 1.33),
+        humidity: item.main.humidity,
+      };
+      if (!acc[date]) {
+        // eslint-disable-next-line no-param-reassign
+        acc[date] = [];
+      }
+      acc[date].push(entry);
+      return acc;
+    }, {});
+    dispatch({ type: FETCHING_WEATHER_FULFILLED, payload: { groupedWeatherData } });
+  } catch (e) {
+    dispatch({ type: FETCH_WEATHER_REJECTED, payload: { e } });
+  }
 };
-
